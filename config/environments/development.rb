53a2a042e0a318ad8a51fee8a9be0aca550eb032
Semyz Serhii <semizserhii@gmail.com>
2019-07-25 19:02:32 +0300

    forgot password(don't work)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,6 +8,17 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "example.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password:ENV["GMAIL_PASSWORD"]
+  }
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
   # Show full error reports.
   config.consider_all_requests_local = true
 
@@ -16,8 +27,6 @@ Rails.application.configure do
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
     config.action_controller.perform_caching = true
     config.action_controller.enable_fragment_cache_logging = true
-    config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-
     config.cache_store = :memory_store
     config.public_file_server.headers = {
       'Cache-Control' => "public, max-age=#{2.days.to_i}"
@@ -60,3 +69,4 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   devise_for :authenticates
 
   root 'salons#index'
+
+  mount LetterOpenerWeb::Engine, at: "/letter_opener"
 end
+
