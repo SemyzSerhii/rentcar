f58e47248dc257e7e4dff1799f84c7e81b2cfd99
Semyz Serhii <semizserhii@gmail.com>
2019-07-26 15:17:31 +0300

    fix reset password

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
 
   root 'salons#index'
 
-  mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
 end
 
