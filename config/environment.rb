2f98422da28c9c697b8112891d44dfe2e2a3e7c1
Semyz Serhii <semizserhii@gmail.com>
2019-07-29 11:15:18 +0300

    add reset password and fix style log in sign up and reset password

--- config/environment.rb
+++ config/environment.rb
@@ -4,12 +4,4 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
-ActionMailer::Base.smtp_settings = {
-  address: "smtp.gmail.com",
-  port: 587,
-  domain: "localhost",
-  authentication: "plain",
-  enable_starttls_auto: true,
-  user_name: ENV["GMAIL_USERNAME"],
-  password: ENV["GMAIL_PASSWORD"]
-}
+
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,8 +8,6 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-
   # Show full error reports.
   config.consider_all_requests_local = true
 
@@ -29,14 +27,14 @@ Rails.application.configure do
   end
 
   config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
-  config.action_mailer.delivery_method = :letter_opener
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
