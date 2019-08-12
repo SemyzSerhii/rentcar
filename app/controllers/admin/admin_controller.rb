f6f96f19a9785891a7d75f692305529bbc3f799f
Semyz Serhii <semizserhii@gmail.com>
2019-08-12 10:42:28 +0300

    add admin rights

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,7 +1,15 @@
 class Admin::AdminController < ApplicationController
+  before_action :must_be_admin
   layout 'admin'
   def index
 
   end
 
+
+private
+  def must_be_admin
+    unless current_customer && current_customer.admin?
+      redirect_to root_path, notice: "Your admin?"
+    end
+  end
 end
