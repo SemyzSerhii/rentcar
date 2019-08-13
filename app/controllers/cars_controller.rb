84347e99e916a44ff613c81dba8ef578f39e745f
Semyz Serhii <semizserhii@gmail.com>
2019-08-13 14:56:57 +0300

    add page to car root and fix style

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,6 +1,6 @@
 class CarsController < ApplicationController
   def index
-    @cars = Car.where(salon_id: params[:salon_id]).order(:name).page params[:page]
+    @cars = Car.where(salon_id: params[:salon_id]).order(:brand).page params[:page]
   end
 
   def show
