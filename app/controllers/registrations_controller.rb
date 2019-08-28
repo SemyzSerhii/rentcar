1a10b3997988e0132b27fdd7e7ba2ca3a01a7850
Semyz Serhii <semizserhii@gmail.com>
2019-08-28 10:12:02 +0300

    add croping avatar customer when create

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -17,7 +17,7 @@ class RegistrationsController < Devise::RegistrationsController
         if resource.active_for_authentication?
           set_flash_message! :notice, :signed_up
           sign_up(resource_name, resource)
-           render :crop
+           render :crop if params[:customer][:avatar].present?
            respond_with resource, location: after_sign_up_path_for(resource) && return
         else
           set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
@@ -40,21 +40,7 @@ class RegistrationsController < Devise::RegistrationsController
   # We need to use a copy of the resource because we don't want to change
   # the current user in place.
   def update
-    self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
-    prev_unconfirmed_email = resource.unconfirmed_email if resource.respond_to?(:unconfirmed_email)
-
-    resource_updated = update_resource(resource, account_update_params)
-    yield resource if block_given?
-    if resource_updated
-        render :crop
-        set_flash_message_for_update(resource, prev_unconfirmed_email)
-        bypass_sign_in resource, scope: resource_name if sign_in_after_change_password?
-        respond_with resource, location: after_update_path_for(resource) && return
-    else
-      clean_up_passwords resource
-      set_minimum_password_length
-      respond_with resource
-    end
+    super
   end
 
   # DELETE /resource
